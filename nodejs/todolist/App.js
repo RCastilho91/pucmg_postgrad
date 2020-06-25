e01e6887850e2fe616336aad24ed75d89dacf0aa
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-06-25 13:06:28 -0300

    Further structuring to App.js

--- nodejs/todolist/App.js
+++ nodejs/todolist/App.js
@@ -19,6 +19,34 @@ yards.command({
         }
     },
     handler: function ( argv ){
-        const info = chalk.green.bold.inverse('Creating new task: ');
+        const info = chalk.green.bold.inverse(`Creating new task: ${ argv.name }`);
+        console.log( argv.name );
+        console.log( argv.description );
+    }
+});
+
+yards.command({
+    command: 'remove',
+    describe: 'Remote a task from the todo list',
+    handler: function () {
+        console.log(chalk.red('Removing an existing task'));
     }
 })
+
+yargs.command({
+    command: 'list',
+    describe: 'List all tasks',
+    handler: function () {
+        console.log(chalk.blue('Listing out all tasks'));
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a task from the ToDo list',
+    handler: function () {
+        console.log(chalk.yellow('Reading a task'));
+    }
+})
+
+yargs.parse();
