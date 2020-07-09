4def3fe8bc5740ba480eec21924961725d840cb1
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-07-09 08:43:59 -0300

    adding further Node files

---
+++ nodejs/exemplos/es6.js
@@ -0,0 +1,42 @@
+const name = 'Biharck'
+const age = 32
+const country = 'Brasil'
+
+const pessoa = {
+    name,
+    idade: age,
+    pais : country
+}
+
+// console.log(pessoa)
+
+// const idade = pessoa.idade
+// const pais = pessoa.pais
+
+const {idade, pais, cidade = 'BElO HORIZONTE'} = pessoa
+
+// console.log(idade)
+// console.log(pais)
+// console.log(cidade)
+
+const save = ({name, idade}) => {
+
+    console.log(name)
+    console.log(idade)
+    //realizar save
+}
+
+save(pessoa)
+
+
+// Criar um CLI usando Yargs OK
+// vai receber um parâmetro de entrada que vai ser o código do ativo na bolsa de valores  OK
+// se não receber o ativo, retornar um erro  OK
+// se receber, deve consultar o ativo usando o request OK
+// devolver o valor de abertura, valor de fechamento, maior alta do dia, menor baixa do dia OK
+// mostrar o valores de baixa em vermelho com chalk
+// mostrar valores de alta de azul
+// utilizar arrow function OK
+// utilizar es6 OK
+// utilizar destruct OK
+
--- nodejs/todolist/modulo.js
+++
@@ -1,9 +0,0 @@
-//console.log('estou em -> modulo.js')
-
-const sobrenome = 'Araujo'
-
-const calculoMaluco = (numero, valorIncrementado) =>{
-    return (numero * valorIncrementado + 2) / 10
-}
-
-module.exports = calculoMaluco
