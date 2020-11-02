fbf19ce9dd540511910efcf8886d5df121dab652
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-11-02 14:26:40 -0300

    Adding Angular project

---
+++ Angular/test-app/src/app/my-component/my-component.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  template: `
+    <p>Custom property: {{ customProperty }}</p>
+    <input type="text" [(ngModel)]="customProperty" />
+    <button (click)="handleCustomEvent()">Send output</button>
+    <ul>
+      <li *ngFor="let item of items">
+        <span *ngIf="item !== 'item 3'"
+          [style.background]="item === 'item 2' ? 'blue': 'green'"
+          [ngClass]="item === 'item 2' ? 'color-white': ''">{{item}}</span>
+      </li>
+    </ul>
+  `,
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent {
+  @Input() customProperty: string;
+  @Output() myCustomOutput = new EventEmitter<string>();
+
+  items = ['item 1', 'item 2', 'item 3', 'item 4'];
+  inputValue: string;
+
+  constructor() { }
+
+  handleCustomEvent() {
+    this.myCustomOutput.emit(this.inputValue);
+  }
+
+}
